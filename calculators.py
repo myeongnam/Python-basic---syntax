bf24ddfc857a064771913814290061c4cbec0ae4
myeongnam <sysop0513@gmail.com>
2023-05-22 17:24:19 +0900

    main

---
+++ calculators.py
@@ -0,0 +1,25 @@
+a = 3
+b = 4
+# 덧셈 : + , 빼기 : - , 곱하기 : *, 나누기 : /, 몫 : //, 나머지 %
+
+print(a + b)
+print(a - b)
+print( a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+# 자료의 형변환
+# str(숫자) -> 문자, int(실수) -> 정수
+
+a = 10
+b = 20
+# 결과값이 1020이 나오로고 덧셈을 하여라
+str(a)+str(b)
+print(str(a)+str(b))
+
+c = 233.3333
+# c의 소수부분을 잘라서 출력하라
+print(int(c))
+
+
